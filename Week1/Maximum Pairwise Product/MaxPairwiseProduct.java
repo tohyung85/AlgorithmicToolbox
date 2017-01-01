3585ddbdddbeb59f648da7d9d2e43416391dc539
Joshua <tantohyung@gmail.com>
2017-01-01 18:47:00 +0800

    Improve solution speed and fixed given bugs

--- Week1/Maximum Pairwise Product/MaxPairwiseProduct.java
+++ Week1/Maximum Pairwise Product/MaxPairwiseProduct.java
@@ -15,6 +15,27 @@ public class MaxPairwiseProduct {
         return result;
     }
 
+    static long getMaxPairwiseProductFast(int[] numbers) {
+        long result = 0;
+        int n = numbers.length;
+        int maxIndex1 = -1;
+        int maxIndex2 = -1;
+
+        for(int i = 0; i < n; i++) {
+            if(maxIndex1 == -1 || numbers[i] > numbers[maxIndex1]) {
+                maxIndex1 = i;
+            }
+        }
+
+        for(int i = 0; i< n; i++) {
+            if((maxIndex1 != i) && ((maxIndex2 == -1) || (numbers[i] > numbers[maxIndex2]))) {
+                maxIndex2 = i;
+            }
+        }
+
+        return (long)numbers[maxIndex1] * numbers[maxIndex2];
+    }
+
     public static void main(String[] args) {
         FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
@@ -22,7 +43,7 @@ public class MaxPairwiseProduct {
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
         }
-        System.out.println(getMaxPairwiseProduct(numbers));
+        System.out.println(getMaxPairwiseProductFast(numbers));
     }
 
     static class FastScanner {
