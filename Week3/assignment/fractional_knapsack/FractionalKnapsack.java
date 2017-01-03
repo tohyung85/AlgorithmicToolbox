500bd81ff7012f326b2b093321d75abcd778d755
Joshua <tantohyung@gmail.com>
2017-01-03 22:18:26 +0800

    Complete assignment2 problem2

--- Week3/assignment/fractional_knapsack/FractionalKnapsack.java
+++ Week3/assignment/fractional_knapsack/FractionalKnapsack.java
@@ -3,7 +3,60 @@ import java.util.Scanner;
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double value = 0;
-        //write your code here
+        // Sort followed by greedy algo O(NlgN)
+        int[][] items = new int[values.length][2];
+        for(int i = 0; i < items.length; i++) { // 2D array of items with value and weight data
+            items[i][0] = values[i];
+            items[i][1] = weights[i];            
+        }
+
+        java.util.Arrays.sort(items, new java.util.Comparator<int[]>() { //sort array by value per weight
+            public int compare(int[] a, int[] b) {
+                return Double.compare((double)b[0]/b[1], (double)a[0]/a[1]);
+            }
+        });
+
+        double spaceLeft = (double)capacity;
+
+        for(int i = 0; i < items.length; i++) {
+            double valPerWeight = (double)items[i][0] / items[i][1];
+            double a;
+            if(items[i][1] <= spaceLeft) {
+                a = (double)items[i][1];
+            } else {
+                a = spaceLeft;
+            }
+            value += a * valPerWeight;
+            spaceLeft -= a;
+            if(spaceLeft < 0.0001) return value;
+        }
+
+        // Greedy algo without sorting O(n2)
+        /*
+        double spaceLeft = (double)capacity;
+        while(spaceLeft > 0.0001) {
+            double maxValPerWeight = -1;
+            int maxValPerWeightItem = -1;
+            for(int i = 0; i < values.length; i++) { // Find item with max value per weight
+                if(weights[i] == 0) continue;
+                double valPerWeight = (double)values[i] / weights[i];
+                if(valPerWeight > maxValPerWeight) {
+                    maxValPerWeight = valPerWeight;
+                    maxValPerWeightItem = i;
+                }
+            }
+            if(maxValPerWeightItem == -1) return value;
+            double a;
+            if(weights[maxValPerWeightItem] <= spaceLeft) {
+                a = (double)weights[maxValPerWeightItem];
+            } else {
+                a = spaceLeft;
+            }
+            value += a * maxValPerWeight;
+            spaceLeft -= a;
+            weights[maxValPerWeightItem] -= a;
+        }
+        */
 
         return value;
     }
@@ -18,6 +71,6 @@ public class FractionalKnapsack {
             values[i] = scanner.nextInt();
             weights[i] = scanner.nextInt();
         }
-        System.out.println(getOptimalValue(capacity, values, weights));
+        System.out.printf("%.4f", getOptimalValue(capacity, values, weights));
     }
 } 
