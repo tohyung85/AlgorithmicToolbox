82ce1269eb8401b377e5cd5191a41bcb75d2ad95
Joshua <tantohyung@gmail.com>
2017-01-10 21:30:47 +0800

    Complete assignment 3 problem 2

--- Week4/assignments/majority_element/MajorityElement.java
+++ Week4/assignments/majority_element/MajorityElement.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class MajorityElement {
+    // Divide and Conquer method
+    
     private static int getMajorityElement(int[] a, int left, int right) {
         if (left == right) {
             return -1;
@@ -9,10 +11,52 @@ public class MajorityElement {
         if (left + 1 == right) {
             return a[left];
         }
-        //write your code here
+
+        int mid = left + (right - left) / 2 - 1;
+
+        int leftMajority = getMajorityElement(a, left, mid);
+        int rightMajority = getMajorityElement(a, mid + 1, right);
+
+        int majority = isMajority(a, leftMajority, left, right);
+        if(majority != -1) return leftMajority;
+        majority = isMajority(a, rightMajority, left, right);
+        if(majority != -1) return rightMajority;
+        
         return -1;
     }
 
+    private static int isMajority(int[] a, int num, int left, int right) {
+        if(num == -1) return -1;
+        int count = 0;
+
+        for(int i = left; i < right; i++) {
+            if(a[i] == num) count++;
+        }
+
+        if(count * 2 > right - left) {
+            return num;        
+        } else {
+            return -1;
+        }
+    }
+    /* Use a hasmap for faster implementation!
+    private static int getMajorityElement(int[] a, int left, int right) {
+        // use hashmap
+        int[] sizes = new int[1000000001];
+
+        for(int i = left; i < right; i++) {
+            int index = a[i];
+            sizes[index] += 1;
+        }
+
+        for(int i = left; i < right; i++) {
+            int index = a[i];
+            if(sizes[index] * 2 > a.length) return a[i];
+        }
+
+        return -1;
+    }
+    */
     public static void main(String[] args) {
         FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
