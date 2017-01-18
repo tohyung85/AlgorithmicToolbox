b245666a13e50ddc61213fb93fd0cae9c7aaf5fe
Joshua <tantohyung@gmail.com>
2017-01-18 22:43:52 +0800

    remove unnecessary code

--- Week5/assignment/primitive_calculator/PrimitiveCalculator.java
+++ Week5/assignment/primitive_calculator/PrimitiveCalculator.java
@@ -48,51 +48,6 @@ public class PrimitiveCalculator {
         return sequence;
     }
 
-    // private static List<Integer> minMoves(int n, List<Integer>[] moves) {
-    //     if(n == 1) {
-    //         List<Integer> list = new new ArrayList<Integer>();
-    //         list.add(1);
-    //         return list;
-    //     }
-
-    //     int min;
-    //     int value = n - 1;
-
-    //     if(moves[n-1] != -1) {
-    //         min = moves[n-1] + 1;
-    //     } else {
-    //         min = minMoves(sequence, n, moves) + 1;
-    //     }
-
-    //     if(n % 3 == 0) {
-    //         int a;
-    //         if(moves[n/3] != -1) {
-    //             a = moves[n/3] + 1;
-    //         } else {
-    //             a = minMoves(sequence, n/3, moves) + 1;
-    //         }            
-    //         if(a < min) {
-    //             min = a;
-    //             value = n/3;
-    //         }
-    //     }
-
-    //     if(n % 2 == 0) {
-    //         int b;
-    //         if(moves[n/2] != -1) {
-    //             b = moves[n/2] + 1;
-    //         } else {
-    //             b = minMoves(sequence, n/3, moves) + 1;
-    //         }
-    //         if(b < min) {
-    //             min = b;
-    //             value = n/2;
-    //         }
-    //     }
-
-    //     return min;
-    // }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
